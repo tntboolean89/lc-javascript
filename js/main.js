0066ff7e5e9fc20227d7dc4c11add7dd56807f38
tntboolean89 <me@taniot.net>
2023-02-20 13:30:58 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,84 @@
+ 'use strict';
+// var testName;
+
+
+
+// const nomeVariabile1 = 'gaetano';
+// const nomeVariabile2 = 'rosa';
+
+// console.log('pippo',nomeVariabile1, 'nomeVariabile2',nomeVariabile2);
+// console.log('nomeVariabile2',nomeVariabile2);
+
+// let nomeUtente = 'Roy';
+// console.log(nomeUtente);
+// nomeUtente = 'Ivan';
+// console.log(nomeUtente);
+
+// const firstName = 10;
+// const lastName = 20;
+
+
+// const userName = firstName + lastName;
+// console.log(userName);
+
+// console.log(testName);
+
+// let userName = 'ciao';
+// userName = 'pluto';
+
+
+//let nomeUtente = 'gaetano';
+
+// let piove = true;
+//nomeUtente = 'gaetano';
+
+//console.log(nomeUtente);
+
+// let stringaVirgolette = '9' + '9';
+// console.log(stringaVirgolette);
+
+// let numeroTest = 9 + 9;
+// console.log(numeroTest);
+
+// const firstName = 'Gaetano';
+// const lastName = 'Frascolla';
+
+// const userName = 'Ciao, ' + firstName + ' ' + lastName;
+// //console.log(userName);
+
+// //interpolazione
+
+// const userNameCool = `Ciao, "prova" ${firstName} 
+// ${lastName} 'nuovaprova'`;
+// //console.log(userNameCool);
+
+// const numStringa = 'pippo500' + 500;
+// const numNumero = 10;
+
+// const somma = numStringa + numNumero;
+// console.log(somma);
+
+// const firstName = prompt('Inserisci il tuo nome');
+// const lastName = prompt('Inserisci il tuo cognome');
+// const userAge = prompt('Inserisci i tuoi anni');
+
+// const userName = `Ciao, ${firstName} ${lastName} di ${userAge} anni`;
+// console.log(userName);
+
+// document.querySelector('h1').innerHTML = userName;
+
+const numberOne = Number(prompt('Inserisci il valore 1'));
+const numberTwo = Number(prompt('Inserisci il valore 2'));
+
+const sumTotal = numberOne + numberTwo;
+console.log(sumTotal);
+
+
+
+
+
+
+
+
+
+
